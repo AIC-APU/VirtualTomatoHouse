d274e5638d9fb96c4c5fa4cfca621f86c020aeca
Takenori Adachi <ada@plusplus.jp>
2023-06-08 12:07:05 +0900

    Replace Shader

--- Unity/VirtualTomatoHouse/Assets/Experimental/ChangeCameraShader.cs
+++ Unity/VirtualTomatoHouse/Assets/Experimental/ChangeCameraShader.cs
@@ -7,7 +7,7 @@ public class ChangeCameraShader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Camera>().SetReplacementShader(Shader.Find("IndexTextureLinear"), null);
     }
 
     // Update is called once per frame
