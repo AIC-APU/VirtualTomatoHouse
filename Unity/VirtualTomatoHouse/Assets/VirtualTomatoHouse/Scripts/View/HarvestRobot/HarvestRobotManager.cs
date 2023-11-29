209e7e3a263674d9bae74dd4d208521d32536769
tanno-maker <t.tanno0827@gmail.com>
2023-11-29 18:59:25 +0900

    ロボットアームによる自動収穫機能のコア部を作成

---
+++ Unity/VirtualTomatoHouse/Assets/VirtualTomatoHouse/Scripts/View/HarvestRobot/HarvestRobotManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plusplus.VirtualTomatoHouse.Scripts.View.HarvestRobot
+{
+    public class HarvestRobotManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Unity/VirtualTomatoHouse/Assets/VirtualTomatoHouse/Scripts/View/HarvestRobot/PIDActuator.cs
@@ -0,0 +1,120 @@
+using UnityEngine;
+
+namespace Plusplus.VirtualTomatoHouse.Scripts.View.HarvestRobot
+{
+    public class PIDActuator : MonoBehaviour
+    {
+        [Header("Target")]
+        public Transform Target;
+        public bool IsActive = true;
+        public bool IsReach => _isSideReach && _isHeightReach;
+
+        [Header("Side Distance Parameters")]
+        [SerializeField] private float _sideKp = 1f;
+        [SerializeField] private float _sideKi = 0.01f;
+        [SerializeField] private float _sideKd = 0.1f;
+        [SerializeField] private float _sideThreshold = 0.1f;
+        [SerializeField] private float _sideMaxSpeed = 1f;
+        [SerializeField] private float _sideOffset = 0f;
+
+        private float _sideErrorSum = 0f;
+        private float _sideLastError = 0f;
+
+
+        [Header("Height Distance Parameters")]
+        [SerializeField] private float _heightKp = 1f;
+        [SerializeField] private float _heightKi = 0.01f;
+        [SerializeField] private float _heightKd = 0.1f;
+        [SerializeField] private float _heightThreshold = 0.1f;
+        [SerializeField] private float _heightMaxSpeed = 1f;
+        [SerializeField] private float _heightOffset = 0f;
+
+        private float _heightErrorSum = 0f;
+        private float _heightLastError = 0f;
+
+        private bool _isSideReach = false;
+        private bool _isHeightReach = false;
+
+        void Update()
+        {
+            if (Target == null || IsActive == false) return;
+
+            SideMove(Target.position);
+
+            HeightMove(Target.position);
+        }
+
+        private void SideMove(Vector3 targetPosition)
+        {
+            var offsetVec = transform.right * _sideOffset;
+            var targetVector = targetPosition + offsetVec - transform.position;
+            var error = Vector3.Dot(targetVector, transform.right);
+
+            if (Mathf.Abs(error) > _sideThreshold)
+            {
+                var deltaError = (error - _sideLastError) / Time.deltaTime;
+                _sideLastError = error;
+                _sideErrorSum += error * Time.deltaTime;
+
+                var output = PIDController(error, _sideKp, _sideKi, _sideKd, _sideMaxSpeed, _sideErrorSum, deltaError);
+
+                transform.position += transform.right * output * Time.deltaTime;
+
+                _isSideReach = false;
+            }
+            else
+            {
+                _sideLastError = 0f;
+                _sideErrorSum = 0f;
+                _isSideReach = true;
+            }
+
+        }
+
+        private void HeightMove(Vector3 targetPosition)
+        {
+            var offsetVec = transform.up * _heightOffset;
+            var targetVector = targetPosition + offsetVec - transform.position;
+            var error = Vector3.Dot(targetVector, transform.up);
+
+            if (Mathf.Abs(error) > _heightThreshold)
+            {
+                var deltaError = (error - _heightLastError) / Time.deltaTime;
+                _heightLastError = error;
+                _heightErrorSum += error * Time.deltaTime;
+
+                var output = PIDController(error, _heightKp, _heightKi, _heightKd, _heightMaxSpeed, _heightErrorSum, deltaError);
+
+                //高さを0から1に制限
+                var heihgt = transform.position + transform.up * output * Time.deltaTime;
+                heihgt.y = Mathf.Clamp(heihgt.y, 0f, 1f);
+                transform.position = heihgt;
+
+                _isHeightReach = false;
+            }
+            else
+            {
+                _heightLastError = 0f;
+                _heightErrorSum = 0f;
+
+                _isHeightReach = true;
+            }
+        }
+
+        private float PIDController(float error, float kp, float ki, float kd, float max, float errorsum = 0, float deltaError = 0)
+        {
+            //PID制御
+            var p = error * kp;
+            var i = errorsum * ki;
+            var d = deltaError * kd;
+
+            //出力
+            var output = p + i + d;
+
+            //出力を制限
+            output = Mathf.Clamp(output, -max, max);
+
+            return output;
+        }
+    }
+}
